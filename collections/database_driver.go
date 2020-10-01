3932ecc83797a3aa2a1c43809227ae71c004611f
Mayada Nour <mayada.nour@emc.com>
2020-10-01 15:23:13 +0200

    some liniting fixes

--- collections/database_driver.go
+++ collections/database_driver.go
@@ -1,17 +1,24 @@
+// Package collections This is the package comment,
+// a top-level piece of documentation
+// used to explain things about the package (see json or exp/template)
+// All godoc comments are in this form
+// with no whitespace between them and what they accompany
 package collections
 
-import(
+import (
 	"context"
+	"database-manager/configuration"
 	"fmt"
 	"log"
-	"database-manager/configuration"
 
-    "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// DatabaseClient provide interface to mongodb
 var DatabaseClient mongo.Client
 
+// Database connect to mongodb
 func Database() {
 	// Set client options
 	clientOptions := options.Client().ApplyURI(configuration.Config.DatabaseURL)
--- collections/insight_collection.go
+++ collections/insight_collection.go
@@ -2,35 +2,47 @@ package collections
 
 import (
 	"context"
+	"database-manager/configuration"
 	"fmt"
 	"log"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-	"database-manager/configuration"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type insight struct {
-	dataID string
-	insightID primitive.ObjectID
+// Insight struct holds data id and insight id
+type Insight struct {
+	DataID    string `json:"dataID" bson:"dataID"`
+	InsightID string `json:"isbn" bson:"_id"`
 }
 
-var collection_name string = "insights"
-var collection = DatabaseClient.Database(configuration.Config.DatabaseName).Collection(collection_name)
-func InsertOne(dataID string)  {
-	insight = Insight{dataID} 
+var collectionName string = "insights"
+var collection = DatabaseClient.Database(configuration.Config.DatabaseName).Collection(collectionName)
+
+//InsertOne : inserts new insight
+func InsertOne(dataID string) (*mongo.InsertOneResult, error) {
+	insight := Insight{DataID: dataID}
 	insertResult, err := collection.InsertOne(context.TODO(), insight)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
-	return insertResult
+	return insertResult, nil
 }
 
-func FindOne(insightID string) Insight{
-	filter := bson.D{{insightID }}
-	var insight Insight 
+// FindOne : finds and returns one insight with the id given
+func FindOne(insightID string) (*Insight, error) {
+	objID, err1 := primitive.ObjectIDFromHex(insightID)
+	filter := bson.D{primitive.E{Key: "_id", Value: objID}}
+	if err1 != nil {
+		return nil, err1
+	}
+	var insight *Insight
 	err := collection.FindOne(context.Background(), filter).Decode(&insight)
-	if err != nil { return err }
-	return insight
+	if err != nil {
+		return nil, err
+	}
+	return insight, nil
 }
-
